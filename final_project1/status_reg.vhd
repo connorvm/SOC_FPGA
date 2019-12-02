ba829e44fd662772b4e156b8b60a4b563fe74d5a
Connor Van Meter <akdreamin21@gmail.com>
2019-12-02 16:50:32 -0700

    Status_reg shell

--- final_project1/status_reg.vhd
+++ final_project1/status_reg.vhd
@@ -1,1 +1,37 @@
 This file will be handling the status resgiter logic
+
+-- Author: Connor Van Meter
+-- Lab Partner: Alex Salois
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+use IEEE.std_logic_unsigned.all;
+
+LIBRARY altera;
+USE altera.altera_primitives_components.all;
+
+
+entity status_reg is
+    port(
+        clk            : in  std_logic;                         -- system clock
+        reset          : in  std_logic;                         -- system reset
+        PB             : in  std_logic;                         -- Pushbutton to change state  
+        SW             : in  std_logic_vector(3 downto 0);      -- Switches that determine next state
+        HS_LED_control : in  std_logic;                         -- Software is in control when asserted (=1)
+        SYS_CLKs_sec   : in  std_logic_vector(31 downto 0);     -- Number of system clock cycles in one second
+        Base_rate      : in  std_logic_vector(7 downto 0);      -- base transition time in seconds, fixed-point data type
+        LED_reg        : in  std_logic_vector(7 downto 0);      -- LED register
+        LED            : out std_logic_vector(7 downto 0)       -- LEDs on the DE10-Nano board
+    );
+end entity status_reg;
+
+
+architecture status_arch of status_reg is 
+
+
+    begin
+
+
+end architecture;
