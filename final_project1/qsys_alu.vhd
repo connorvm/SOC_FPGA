c5316698b092ed1f52c63512cd939025c8574c58
Connor Van Meter <akdreamin21@gmail.com>
2019-12-04 18:23:04 -0700

    Increased functionality of qsys_alu. (I think I got the LED stuff done...)

--- final_project1/qsys_alu.vhd
+++ final_project1/qsys_alu.vhd
@@ -31,12 +31,17 @@ end entity qsys_alu;
 architecture qsys_alu_arch of qsys_alu is 
 
 
-    signal show_reg  : std_logic_vector(1 downto 0);
-    signal show_byte : std_logic_vector(7 downto 0);
-    signal a_reg     : std_logic_vector(1 downto 0);
-    signal b_reg     : std_logic_vector(1 downto 0);
-    signal h_reg     : std_logic_vector(1 downto 0);
-    signal l_reg     : std_logic_vector(1 downto 0);
+    signal show_reg     : std_logic_vector(1 downto 0);
+    signal a_reg        : std_logic_vector(1 downto 0);
+    signal b_reg        : std_logic_vector(1 downto 0);
+    signal h_reg        : std_logic_vector(1 downto 0);
+    signal l_reg        : std_logic_vector(1 downto 0);
+
+    signal show_byte    : std_logic_vector(7 downto 0);
+    signal l_byte       : std_logic_vector(7 downto 0);
+    signal ml_byte      : std_logic_vector(7 downto 0);
+    signal mh_byte      : std_logic_vector(7 downto 0);
+    signal h_byte       : std_logic_vector(7 downto 0);
 
     begin
 
@@ -54,13 +59,62 @@ architecture qsys_alu_arch of qsys_alu is
             end case;
 
             case SW(1 downto 0) is
-                when "00" => show_byte <= 
-                
-                    
-            
-                when others =>
+                when "00" => show_byte <= l_byte  --show low byte (7 downto 0)
+                when "01" => show_byte <= ml_byte --show mid-low byte (15 downto 8)
+                when "10" => show_byte <= mh_byte --show mid-high byte (23 downto 16)
+                when "11" => show_byte <= h_byte  --show high byte (31 downto 24)
+                when others => show_byte <= x"00";
             end case ;
+
+            if show_byte = l_byte then
+                if show_reg = a_reg then
+                    LED <= a(7 downto 0);
+                elsif show_reg = b_reg then
+                    LED <= b(7 downto 0);
+                elsif show_reg = l_reg then
+                    LED <= result_l(7 downto 0);
+                elsif show_reg = h_reg then
+                    LED <= result_h(7 downto 0);
+                end if ;
+                
+            elsif show_byte = ml_byte then
+                if show_reg = a_reg then
+                    LED <= a(15 downto 8);
+                elsif show_reg = b_reg then
+                    LED <= b(15 downto 8);
+                elsif show_reg = l_reg then
+                    LED <= result_l(15 downto 8);
+                elsif show_reg = h_reg then
+                    LED <= result_h(15 downto 8);
+                end if ;
+
+            elsif show_byte = mh_byte then
+                if show_reg = a_reg then
+                    LED <= a(23 downto 16);
+                elsif show_reg = b_reg then
+                    LED <= b(23 downto 16);
+                elsif show_reg = l_reg then
+                    LED <= result_l(23 downto 16);
+                elsif show_reg = h_reg then
+                    LED <= result_h(23 downto 16);
+                end if ;
+
+            elsif show_byte = h_byte then
+                if show_reg = a_reg then
+                    LED <= a(31 downto 24);
+                elsif show_reg = b_reg then
+                    LED <= b(31 downto 24);
+                elsif show_reg = l_reg then
+                    LED <= result_l(31 downto 24);
+                elsif show_reg = h_reg then
+                    LED <= result_h(31 downto 24);
+                end if ;
+            
+            else
+                LED <= "00000000";
+            end if ;
         end process;
+        
 
 
 
