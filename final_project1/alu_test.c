d02adb76dc7a93e2cd30eb1c20f7c180a83ad051
Connor Van Meter <akdreamin21@gmail.com>
2019-12-10 22:10:25 -0700

    Added formatting for the C program demonstration. Had to hard code in the values. Not able to test, so hopefully it works.

--- final_project1/alu_test.c
+++ final_project1/alu_test.c
@@ -1,5 +1,5 @@
 //Authors: Connor Van Meter and Alex Salois
-//This C-Program will demonstarte the opcode operations.
+//This C-Program will demonstrate the opcode operations.
 //Needs to access the registers using mmap() in Linux.
 //Needs to run automatically on boot-up using systemd.
 
@@ -123,33 +123,312 @@ int main()
     printf("                           %08x\n",*rl);
     printf("STATUS: 0x%p", status);
     printf("                       %08x\n",*status);
+
+    // int count = 20;
     
-    // printf("**************************\n");
-    // printf("register addresses\n");
-    // printf("**************************\n");
-    // printf("hs_led_control address: 0x%p\n", a);
+    // for (int i = 1; i < count; i++)
+    // {
+    //     printf("Case %d     R0      R1      R2          R4          R3          R4+3        R5\n", i);
+    //     printf("IRV:        %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, *opcode, *rh, *rl, *status);
+    //     printf("ABW:        %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, *opcode, *rh, *rl, *status);
+    //     printf("OFW:        %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, *opcode, *rh, *rl, *status);
+    // }
     
-    //  // Print the other register addresses here
-   
-    // printf("**************************\n");
-    // printf("register values\n");
-    // printf("**************************\n");
-    // printf("hs_led_control: 0x%08x\n", *b);
+    signed initial_a = 0;
+    signed initial_b = 0;
+    signed initial_RH = 0;
+    signed initial_RL = 0;
+    signed initial_op = 0;
+    signed initial_st = 0;
 
+    //CASE 1: R = A + B,   A = 100 , B = 100
+    *a = 100;
+    *b = 100;
+    *opcode = 1; //add
+    printf("Case 1     R0      R1      R2          R4          R3          R4+3        R5\n");
+    printf("IRV:       %d      %d      %08x        %08x        %08x        %d          %08x\n", initial_a, initial_b, initial_op, initial_RH, initial_RL, initial_st);
+    printf("ABW:       %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, initial_op, initial_RH, initial_RL, initial_st);
+    printf("OFW:       %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, *opcode, *rh, *rl, *status);
+    initial_a = *a;
+    initial_b = *b;
+    initial_RH = *rh;
+    initial_RL = *rl;
+    initial_op = *opcode;
+    initial_st = *status; 
 
-    // printf("**************************\n");
-    // printf("register values\n");
-    // printf("**************************\n");
-    // printf("hs_led_control: 0x%08x\n", *c);
+    //CASE 2: R = A + B,   A = 100 , B = 100
+    *a = 100;
+    *b = 100;
+    *opcode = 1; //add
+    printf("Case 2     R0      R1      R2          R4          R3          R4+3        R5\n");
+    printf("IRV:       %d      %d      %08x        %08x        %08x        %d          %08x\n", initial_a, initial_b, initial_op, initial_RH, initial_RL, initial_st);
+    printf("ABW:       %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, initial_op, initial_RH, initial_RL, initial_st);
+    printf("OFW:       %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, *opcode, *rh, *rl, *status);
+    initial_a = *a;
+    initial_b = *b;
+    initial_RH = *rh;
+    initial_RL = *rl;
+    initial_op = *opcode;
+    initial_st = *status;
 
+    //CASE 3: R = A + B,   A = 0x7FFFFFFF , B = 1
+    *a = 0x7FFFFFFF;
+    *b = 1;
+    *opcode = 1; //add
+    printf("Case 3     R0      R1      R2          R4          R3          R4+3        R5\n");
+    printf("IRV:       %d      %d      %08x        %08x        %08x        %d          %08x\n", initial_a, initial_b, initial_op, initial_RH, initial_RL, initial_st);
+    printf("ABW:       %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, initial_op, initial_RH, initial_RL, initial_st);
+    printf("OFW:       %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, *opcode, *rh, *rl, *status);
+    initial_a = *a;
+    initial_b = *b;
+    initial_RH = *rh;
+    initial_RL = *rl;
+    initial_op = *opcode;
+    initial_st = *status; 
 
-    // printf("**************************\n");
-    // printf("register values\n");
-    // printf("**************************\n");
-    // printf("hs_led_control: 0x%08x\n", *opcode);
-    // Print the other register values here
+    //CASE 4: R = A - B,   A = 1000 , B = 1000
+    *a = 1000;
+    *b = 1000;
+    *opcode = 2; //subtract
+    printf("Case 4     R0      R1      R2          R4          R3          R4+3        R5\n");
+    printf("IRV:       %d      %d      %08x        %08x        %08x        %d          %08x\n", initial_a, initial_b, initial_op, initial_RH, initial_RL, initial_st);
+    printf("ABW:       %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, initial_op, initial_RH, initial_RL, initial_st);
+    printf("OFW:       %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, *opcode, *rh, *rl, *status);
+    initial_a = *a;
+    initial_b = *b;
+    initial_RH = *rh;
+    initial_RL = *rl;
+    initial_op = *opcode;
+    initial_st = *status;
+
+    //CASE 5: R = A - B,   A = 1000 , B = 5000
+    *a = 1000;
+    *b = 5000;
+    *opcode = 2; //subtract
+    printf("Case 5     R0      R1      R2          R4          R3          R4+3        R5\n");
+    printf("IRV:       %d      %d      %08x        %08x        %08x        %d          %08x\n", initial_a, initial_b, initial_op, initial_RH, initial_RL, initial_st);
+    printf("ABW:       %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, initial_op, initial_RH, initial_RL, initial_st);
+    printf("OFW:       %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, *opcode, *rh, *rl, *status);
+    initial_a = *a;
+    initial_b = *b;
+    initial_RH = *rh;
+    initial_RL = *rl;
+    initial_op = *opcode;
+    initial_st = *status;
+
+    //CASE 6: R = A - B,   A = 0x80000000 , B = 1
+    *a = 0x80000000;
+    *b = 1;
+    *opcode = 2; //subtract
+    printf("Case 6     R0      R1      R2          R4          R3          R4+3        R5\n");
+    printf("IRV:       %d      %d      %08x        %08x        %08x        %d          %08x\n", initial_a, initial_b, initial_op, initial_RH, initial_RL, initial_st);
+    printf("ABW:       %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, initial_op, initial_RH, initial_RL, initial_st);
+    printf("OFW:       %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, *opcode, *rh, *rl, *status);
+    initial_a = *a;
+    initial_b = *b;
+    initial_RH = *rh;
+    initial_RL = *rl;
+    initial_op = *opcode;
+    initial_st = *status;
+
+    //CASE 7: R = A * B,   A = 0x0FFFFFFF , B = 0x0FFFFFFF
+    *a = 0x0FFFFFFF;
+    *b = 0x0FFFFFFF;
+    *opcode = 3; //multiply
+    printf("Case 7     R0      R1      R2          R4          R3          R4+3        R5\n");
+    printf("IRV:       %d      %d      %08x        %08x        %08x        %d          %08x\n", initial_a, initial_b, initial_op, initial_RH, initial_RL, initial_st);
+    printf("ABW:       %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, initial_op, initial_RH, initial_RL, initial_st);
+    printf("OFW:       %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, *opcode, *rh, *rl, *status);
+    initial_a = *a;
+    initial_b = *b;
+    initial_RH = *rh;
+    initial_RL = *rl;
+    initial_op = *opcode;
+    initial_st = *status;
+
+    //CASE 8: R = A * B,   A = 7000 , B = -500
+    *a = 7000;
+    *b = -500;
+    *opcode = 3; //multiply
+    printf("Case 8     R0      R1      R2          R4          R3          R4+3        R5\n");
+    printf("IRV:       %d      %d      %08x        %08x        %08x        %d          %08x\n", initial_a, initial_b, initial_op, initial_RH, initial_RL, initial_st);
+    printf("ABW:       %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, initial_op, initial_RH, initial_RL, initial_st);
+    printf("OFW:       %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, *opcode, *rh, *rl, *status);
+    initial_a = *a;
+    initial_b = *b;
+    initial_RH = *rh;
+    initial_RL = *rl;
+    initial_op = *opcode;
+    initial_st = *status;
+
+    //CASE 9: R = A * B,   A = 7000 , B = 0
+    *a = 7000;
+    *b = 0;
+    *opcode = 3; //multiply
+    printf("Case 9     R0      R1      R2          R4          R3          R4+3        R5\n");
+    printf("IRV:       %d      %d      %08x        %08x        %08x        %d          %08x\n", initial_a, initial_b, initial_op, initial_RH, initial_RL, initial_st);
+    printf("ABW:       %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, initial_op, initial_RH, initial_RL, initial_st);
+    printf("OFW:       %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, *opcode, *rh, *rl, *status);
+    initial_a = *a;
+    initial_b = *b;
+    initial_RH = *rh;
+    initial_RL = *rl;
+    initial_op = *opcode;
+    initial_st = *status;
+
+    //CASE 10: R = B - 1,   A = 0 , B = 16
+    *a = 0;
+    *b = 16;
+    *opcode = 4; //decrement B
+    printf("Case 10    R0      R1      R2          R4          R3          R4+3        R5\n");
+    printf("IRV:       %d      %d      %08x        %08x        %08x        %d          %08x\n", initial_a, initial_b, initial_op, initial_RH, initial_RL, initial_st);
+    printf("ABW:       %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, initial_op, initial_RH, initial_RL, initial_st);
+    printf("OFW:       %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, *opcode, *rh, *rl, *status);
+    initial_a = *a;
+    initial_b = *b;
+    initial_RH = *rh;
+    initial_RL = *rl;
+    initial_op = *opcode;
+    initial_st = *status;
+
+    //CASE 11: R = B - 1,   A = 0 , B = 0x80000000
+    *a = 0;
+    *b = 0x80000000;
+    *opcode = 4; //decrement B
+    printf("Case 11    R0      R1      R2          R4          R3          R4+3        R5\n");
+    printf("IRV:       %d      %d      %08x        %08x        %08x        %d          %08x\n", initial_a, initial_b, initial_op, initial_RH, initial_RL, initial_st);
+    printf("ABW:       %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, initial_op, initial_RH, initial_RL, initial_st);
+    printf("OFW:       %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, *opcode, *rh, *rl, *status);
+    initial_a = *a;
+    initial_b = *b;
+    initial_RH = *rh;
+    initial_RL = *rl;
+    initial_op = *opcode;
+    initial_st = *status;
+
+    //CASE 12: R = B - 1,   A = 0 , B = 0
+    *a = 0;
+    *b = 16;
+    *opcode = 4; //decrement B
+    printf("Case 12    R0      R1      R2          R4          R3          R4+3        R5\n");
+    printf("IRV:       %d      %d      %08x        %08x        %08x        %d          %08x\n", initial_a, initial_b, initial_op, initial_RH, initial_RL, initial_st);
+    printf("ABW:       %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, initial_op, initial_RH, initial_RL, initial_st);
+    printf("OFW:       %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, *opcode, *rh, *rl, *status);
+    initial_a = *a;
+    initial_b = *b;
+    initial_RH = *rh;
+    initial_RL = *rl;
+    initial_op = *opcode;
+    initial_st = *status;
+
+    //CASE 13: R = A,   A = 5 , B = 0
+    *a = 5;
+    *b = 0;
+    *opcode = 5; //move A
+    printf("Case 13    R0      R1      R2          R4          R3          R4+3        R5\n");
+    printf("IRV:       %d      %d      %08x        %08x        %08x        %d          %08x\n", initial_a, initial_b, initial_op, initial_RH, initial_RL, initial_st);
+    printf("ABW:       %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, initial_op, initial_RH, initial_RL, initial_st);
+    printf("OFW:       %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, *opcode, *rh, *rl, *status);
+    initial_a = *a;
+    initial_b = *b;
+    initial_RH = *rh;
+    initial_RL = *rl;
+    initial_op = *opcode;
+    initial_st = *status;
+
+    //CASE 14: R = A,   A = 0 , B = 0
+    *a = 0;
+    *b = 0;
+    *opcode = 5; //move A
+    printf("Case 14    R0      R1      R2          R4          R3          R4+3        R5\n");
+    printf("IRV:       %d      %d      %08x        %08x        %08x        %d          %08x\n", initial_a, initial_b, initial_op, initial_RH, initial_RL, initial_st);
+    printf("ABW:       %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, initial_op, initial_RH, initial_RL, initial_st);
+    printf("OFW:       %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, *opcode, *rh, *rl, *status);
+    initial_a = *a;
+    initial_b = *b;
+    initial_RH = *rh;
+    initial_RL = *rl;
+    initial_op = *opcode;
+    initial_st = *status;
+
+    //CASE 15: R = A,   A = -5 , B = 0
+    *a = -5;
+    *b = 0;
+    *opcode = 5; //move A
+    printf("Case 15    R0      R1      R2          R4          R3          R4+3        R5\n");
+    printf("IRV:       %d      %d      %08x        %08x        %08x        %d          %08x\n", initial_a, initial_b, initial_op, initial_RH, initial_RL, initial_st);
+    printf("ABW:       %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, initial_op, initial_RH, initial_RL, initial_st);
+    printf("OFW:       %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, *opcode, *rh, *rl, *status);
+    initial_a = *a;
+    initial_b = *b;
+    initial_RH = *rh;
+    initial_RL = *rl;
+    initial_op = *opcode;
+    initial_st = *status;
+
+    //CASE 16: A <-> B,   A = -1 , B = 0
+    *a = -1;
+    *b = 0;
+    *opcode = 6; //swap
+    printf("Case 16    R0      R1      R2          R4          R3          R4+3        R5\n");
+    printf("IRV:       %d      %d      %08x        %08x        %08x        %d          %08x\n", initial_a, initial_b, initial_op, initial_RH, initial_RL, initial_st);
+    printf("ABW:       %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, initial_op, initial_RH, initial_RL, initial_st);
+    printf("OFW:       %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, *opcode, *rh, *rl, *status);
+    initial_a = *a;
+    initial_b = *b;
+    initial_RH = *rh;
+    initial_RL = *rl;
+    initial_op = *opcode;
+    initial_st = *status;
+
+    //CASE 17: R = A AND B,   A =  , B = 
+    *a = ;
+    *b = ;
+    *opcode = 7; //custom -> AND --- SET THE N FLAG
+    printf("Case 17    R0      R1      R2          R4          R3          R4+3        R5\n");
+    printf("IRV:       %d      %d      %08x        %08x        %08x        %d          %08x\n", initial_a, initial_b, initial_op, initial_RH, initial_RL, initial_st);
+    printf("ABW:       %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, initial_op, initial_RH, initial_RL, initial_st);
+    printf("OFW:       %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, *opcode, *rh, *rl, *status);
+    initial_a = *a;
+    initial_b = *b;
+    initial_RH = *rh;
+    initial_RL = *rl;
+    initial_op = *opcode;
+    initial_st = *status;
+
+    //CASE 18: R = A AND B,   A = 1 , B = 0
+    *a = 1;
+    *b = 0;
+    *opcode = 7; //custom -> AND --- SET THE Z FLAG
+    printf("Case 18    R0      R1      R2          R4          R3          R4+3        R5\n");
+    printf("IRV:       %d      %d      %08x        %08x        %08x        %d          %08x\n", initial_a, initial_b, initial_op, initial_RH, initial_RL, initial_st);
+    printf("ABW:       %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, initial_op, initial_RH, initial_RL, initial_st);
+    printf("OFW:       %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, *opcode, *rh, *rl, *status);
+    initial_a = *a;
+    initial_b = *b;
+    initial_RH = *rh;
+    initial_RL = *rl;
+    initial_op = *opcode;
+    initial_st = *status;
+
+    //CASE 19: R = A AND B,   A = 0xC0DE  , B = 467
+    *a = 0xC0DE;
+    *b = 467;
+    *opcode = 7; //custom -> AND
+    printf("Case 19    R0      R1      R2          R4          R3          R4+3        R5\n");
+    printf("IRV:       %d      %d      %08x        %08x        %08x        %d          %08x\n", initial_a, initial_b, initial_op, initial_RH, initial_RL, initial_st);
+    printf("ABW:       %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, initial_op, initial_RH, initial_RL, initial_st);
+    printf("OFW:       %d      %d      %08x        %08x        %08x        %d          %08x\n", *a, *b, *opcode, *rh, *rl, *status);
+    initial_a = *a;
+    initial_b = *b;
+    initial_RH = *rh;
+    initial_RL = *rl;
+    initial_op = *opcode;
+    initial_st = *status;
 
     
+    
+
     /*
     // set the component into software control mode
     //*hs_led_control = 1;
