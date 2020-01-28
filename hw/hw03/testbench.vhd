594ffbdc813692274f621e37d136f85cde1052a8
Alexander Salois <asalois.scholar@gmail.com>
2020-01-28 12:53:23 -0700

    made testbench file

---
+++ hw/hw03/testbench.vhd
@@ -0,0 +1,21 @@
+-- Authors: Connor Van Meter & Alex Salois
+-- EELE 468
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+use STD.textio.all;
+use IEEE.std_logic_textio.all;
+
+entity add_1 is
+  port (Input  : in  std_logic_vector (7 downto 0);
+	Output : out std_logic_vector (7 downto 0));
+end entity;
+
+architecture add_1_arch of add_1 is
+
+  begin
+
+
+
+end architecture;
