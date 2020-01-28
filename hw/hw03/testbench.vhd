38f38af6a30135c26ff784ee39b4d825d667820b
Connor Van Meter <akdreamin21@gmail.com>
2020-01-28 13:36:34 -0700

    Testbench edits

--- hw/hw03/testbench.vhd
+++ hw/hw03/testbench.vhd
@@ -13,18 +13,30 @@ end;
 
 architecture testbench_arch of testbench is
 component add_1 is
-  port (  input  : in  std_logic_vector (7 downto 0);
-	        output : out std_logic_vector (7 downto 0));
+  port (input_vector  : in  std_logic_vector (7 downto 0);
+	output_vector : out std_logic_vector (7 downto 0));
 end component;
 
-signal input_sig : std_logic_vector (7 downto 0) := x'00';
-signal output_sig : std_logic_vector (7 downto 0) := x'00';
+signal input_sig  : std_logic_vector(7 downto 0) := "00000000";
+signal output_sig : std_logic_vector(7 downto 0) := "00000000";
 
   begin
 
+  --input_sig  <= "00000000";
+  --output_sig <= "00000000"; 
+
 dut : add_1
    port map(
-      input => input_sig,
-      output => outut_sig);
+      input_vector  => input_sig,
+      output_vector => output_sig);
+
+process 
+ begin
+
+wait for 10ns; input_sig <= "00110011";
+wait for 10ns; input_sig <= "11001100";
+
+end process;
+
 
 end architecture;
