f07720157f22e71584d090526b0ea199b86d3b71
Alexander Salois <asalois.scholar@gmail.com>
2020-01-28 13:16:12 -0700

    more TB

--- hw/hw03/testbench.vhd
+++ hw/hw03/testbench.vhd
@@ -11,29 +11,20 @@ entity testbench is
   PORT ( count : BUFFER bit_vector(8 downto 1));
 end; 
 
+architecture testbench_arch of testbench is
 component add_1 is
-  port (  Input  : in  std_logic_vector (7 downto 0);
-	        Output : out std_logic_vector (7 downto 0));
+  port (  input  : in  std_logic_vector (7 downto 0);
+	        output : out std_logic_vector (7 downto 0));
 end component;
 
-signal clk   : bit := '0';
-signal input : std_logic_vector (7 downto 0) := x'00' 
-
-dut : add_1
-   port map(
-      coun => count,
-      clk => clk,
-      );
-
-clock : PROCESS
-   begin
-   wait for 10 ns; clk  <= not clk;
-end PROCESS clock;
-
-architecture add_1_arch of add_1 is
+signal input_sig : std_logic_vector (7 downto 0) := x'00';
+signal output_sig : std_logic_vector (7 downto 0) := x'00';
 
   begin
 
-
+dut : add_1
+   port map(
+      input => input_sig,
+      output => outut_sig);
 
 end architecture;
