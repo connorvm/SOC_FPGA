a622d219668e4f26291f7c3a392054d605a3da30
Connor Van Meter <akdreamin21@gmail.com>
2020-01-28 13:05:24 -0700

    Compiled add_1 with no errors

--- hw/hw03/add_1.vhd
+++ hw/hw03/add_1.vhd
@@ -2,20 +2,25 @@
 -- EELE 468
 
 library IEEE;
-use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
+use IEEE.std_logic_1164.all;
 use STD.textio.all;
 use IEEE.std_logic_textio.all;
 
 entity add_1 is
-  port (Input  : in  std_logic_vector (7 downto 0);
-	Output : out std_logic_vector (7 downto 0));
+  port (input_vector  : in  std_logic_vector (7 downto 0);
+	output_vector : out std_logic_vector (7 downto 0));
 end entity;
 
 architecture add_1_arch of add_1 is
 
+  signal input : unsigned(7 downto 0);
+  signal output : unsigned(7 downto 0);
+
   begin
 
-
+    input  <= unsigned(input_vector);
+    output <= input + "00000001";
+    output_vector <= std_logic_vector(output);
 
 end architecture;
