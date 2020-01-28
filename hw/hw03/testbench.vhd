59820b4bcae7e43ebe6433ec5d9e0aeea26d0ce7
Alexander Salois <asalois.scholar@gmail.com>
2020-01-28 13:06:02 -0700

    begin to set up TB

--- hw/hw03/testbench.vhd
+++ hw/hw03/testbench.vhd
@@ -7,10 +7,28 @@ use IEEE.numeric_std.all;
 use STD.textio.all;
 use IEEE.std_logic_textio.all;
 
-entity add_1 is
-  port (Input  : in  std_logic_vector (7 downto 0);
-	Output : out std_logic_vector (7 downto 0));
-end entity;
+entity testbench is
+  PORT ( count : BUFFER bit_vector(8 downto 1));
+end; 
+
+component add_1 is
+  port (  Input  : in  std_logic_vector (7 downto 0);
+	        Output : out std_logic_vector (7 downto 0));
+end component;
+
+signal clk   : bit := '0';
+signal input : std_logic_vector (7 downto 0) := x'00' 
+
+dut : add_1
+   port map(
+      coun => count,
+      clk => clk,
+      );
+
+clock : PROCESS
+   begin
+   wait for 10 ns; clk  <= not clk;
+end PROCESS clock;
 
 architecture add_1_arch of add_1 is
 
